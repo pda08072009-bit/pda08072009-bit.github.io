33d583e5ee54014e7c9021b3737b3c1eaac573e9
pda08072009-bit <pda08072009@gmail.com>
2025-12-15 17:03:59 +0300

    Create .py

---
+++ .py
@@ -0,0 +1,23 @@
+import asyncio
+from dotenv import load_dotenv
+import os 
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message 
+from aiogram.filters import CommandStart
+
+load_dotenv()
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+  await message.answer("Привет! Я твой бот)
+
+async def main():
+  await dp.start_polling(bot)
+
+if __name__ == "__main__":
+  asyncio.run(main())
